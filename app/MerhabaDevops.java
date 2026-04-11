200c0a0f20e97b1e3c6b5df4f201eec90dfcbd73
Ba2g <batusukru-gudek@hotmail.com>
2026-04-11 13:32:49 +0300

    Kontrolden geçti yeşile dondurduk

--- app/MerhabaDevops.java
+++ app/MerhabaDevops.java
@@ -1,7 +1,7 @@
 public class MerhabaDevops {
     public static void main(String[] args) {
         System.out.println("-----------------------------------------");
-        System.out.println("MERHABA DUNYA PIPELINE BASARIYLA CALISTI!");
+        System.out.println("DEVOPS PIPELINE BASARIYLA CALISTI!");
         System.out.println("Otomasyon tamamlandi, sistem guncellendi.");
         System.out.println("-----------------------------------------");
     }
